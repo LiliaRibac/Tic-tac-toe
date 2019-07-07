dc56a485049d1177f0bab024a348d81bab465870
LiliaRibac <ribac.lilia@gmai.com>
2019-07-07 17:11:02 -0500

    see x on the page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+constructor(props){
+  super(props)
+  this.state ={
+    board:Array(9).fill(null)
+  }
+}
+
+handleClick(index){
+  let newBoard = this.state.board
+  newBoard[index] ='X'
+ this.setState ({
+board:newBoard
+ })
+  console.log(this.state.board)
+  //console.log(index)
+}
+
+render (){
+  const Box = this.state.board.map((box,index) => <div className ="box"key={index} onClick={()=> this.handleClick(index)}>{box}</div>)
+  return (  
+
+    <div className="container">
+      <h1>Tic tac-toe app</h1>
+      <div className="board">
+      {Box}
+    </div>
+    
     </div>
   );
+  } 
 }
 
 export default App;
